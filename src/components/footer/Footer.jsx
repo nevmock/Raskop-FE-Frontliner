ba159300e9025113fd163e217e379ad4071186ac
Hauzan Rafi Attallah <rafihauzan42@gmail.com>
2024-11-23 15:20:21 +0700

    Update app title and modify footer and navbar styles to use green theme

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -4,7 +4,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="bg-white  shadow ">
+    <footer className="bg-green-main shadow ">
       <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
         <div className="sm:flex sm:items-center sm:justify-between">
           <Link href="/">
@@ -17,7 +17,7 @@ const Footer = () => {
               className="w-[50px] h-[27px] sm:w-[70px] sm:h-[35px] md:w-[131px] md:h-[72px] cursor-pointer object-contain"
             />
           </Link>
-          <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0">
+          <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-white sm:mb-0">
             <li>
               <a href="#" className="hover:underline me-4 md:me-6">
                 About
@@ -41,7 +41,7 @@ const Footer = () => {
           </ul>
         </div>
         <hr className="my-6 border-gray-200 sm:mx-auto lg:my-8" />
-        <span className="block text-sm text-gray-500 sm:text-center">
+        <span className="block text-sm text-white sm:text-center">
           © 2024{" "}
           <a href="https://flowbite.com/" className="hover:underline">
             Nevmock™
