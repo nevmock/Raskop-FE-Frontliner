8d970a5d3efc22e74d1c3609b32a9816b896d4ba
aryafirmansyah2 <aryafirmansyah200404@example.com>
2025-01-18 18:33:06 +0700

    RSKP-3 | UI Reservasi Website | User

--- src/app/reservation/[id]/page.jsx
+++ src/app/reservation/[id]/page.jsx
@@ -1,5 +1,13 @@
 import React from 'react';
 
+export async function generateStaticParams() {
+  const ids = ['1', '2', '3', '4', '5', '6'];
+
+  return ids.map((id) => ({
+    id,
+  }));
+}
+
 const FormReservation = () => {
   return <div>FormReservation</div>;
 };
